91bf63c16e0f7dd9800d1077daf262cffbfb4cb5
Helio Loureiro <helio@loureiro.eng.br>
2024-07-28 17:30:21 +0200

    Improved script

--- scripts/filter_out_pauta_content.py
+++ scripts/filter_out_pauta_content.py
@@ -1,8 +1,12 @@
 #! /usr/bin/python3
 
+import os
+import sys
+import time
 import argparse
 import requests
 import re
+import json
 
 GITHUB = "https://github.com"
 PAUTAS = f"{GITHUB}/helioloureiro/canalunixloadon/tree/master/pautas"
@@ -26,41 +30,67 @@ def getHref(line):
 def unblob(url):
     return re.sub("/blob", "", url)
 
+def getJsonFromLine(line: str) -> str:
+    line = re.sub("</.*>", "", line)
+    line = re.sub(".*>", "", line)
+    return json.loads(line)
+
 class Pautas(object):
     def __init__(self): None
 
     def GetLatest(self):
         links = []
         pautas = curl(PAUTAS)
+        dataJson = {}
         for line in pautas.splitlines():
-            if not re.search("js-navigation-open Link--primary", line):
+            if not re.search("react-app.embeddedData", line):
                 continue
-            if re.search("template.md", line):
-                continue
-            line = getHref(line)
-            links.append(line)
-        self.latest = sorted(links)[-1]
-        self.latest = unblob(self.latest)
-        self.latestURL = f"{RAWGITHUB}{self.latest}"
+            dataJson = getJsonFromLine(line)
+            break
+        print(json.dumps(dataJson, indent=4))
+
+        content = []
+        for line in dataJson["payload"]["tree"]["items"]:
+            if re.search(r"\d+.md", line["name"]):
+                content.append(line["name"])
+        content = sorted(content)
+        print(content)
+
+        self.latest = content[-1]
+        self.latestURL = f"{RAWGITHUB}/helioloureiro/canalunixloadon/master/pautas/{self.latest}"
         print("latest  :", self.latestURL)
         #print("expected: https://raw.githubusercontent.com/helioloureiro/canalunixloadon/master/pautas/20220210.md")
 
-    def GetContentFromRaw(self, url):
+    def GetContentFromRaw(self, url : str) -> dict:
         body = curl(url)
-        content = []
-        for line in body.splitlines():
-            if line.startswith("*"):
-                content.append(line)
-                #print(line)
-        self.lastContent = content
+        sections = {}
+        topics = body.split("\n\n")
+        for tpc in topics:
+            lines = tpc.splitlines()
+            title = None
+            if re.search("========", lines[1]):
+                title = lines[0]
+            elif re.search("=========", lines[2]):
+                title = lines[1]
+
+            if title is None:
+                print("Empty body:", tpc) 
+                continue
+            print("Adding section:", title)
+
+            content = []
+            for line in lines:
+                if line.startswith("*"):
+                    content.append(line)
+                    continue
+            sections[title] = content
+        return sections
 
 
     def FetchLatestContent(self):
         self.GetLatest()
-        self.GetContentFromRaw(self.latestURL)
-        return self.lastContent
-
-
+        data = self.GetContentFromRaw(self.latestURL)
+        return data
 
 class NewFilter(object):
     def __init__(self):
@@ -84,17 +114,43 @@ class NewFilter(object):
             self.webURL = pauta.latestURL
 
 
-    def Out(self):
-        content = []
-        if self.contentFile is not None:
-            with open(self.contentFile) as cFD:
-                for line in cFD.readlines():
-                    content.append(line)
-        req = requests.get(self.webURL)
-        for line in req.text.splitlines():
-            if not line in content:
-                print(line)
+    def CalculateNextDay(self) -> str:
+        lastDay = os.path.basename(self.webURL)
+        lastDay = re.sub(r"\.md", "", lastDay)
+        year = lastDay[:4]
+        month = lastDay[4:6]
+        day = lastDay[6:]
+        #print("Lastday:", lastDay)
+        #print("year:" ,year)
+        #print("month:", month)
+        #print("day:", day)
+        timestamp = time.strftime("%H:%M:%S", time.gmtime())
+        hour, minute, second = timestamp.split(":")
+        tup = (int(year), int(month), int(day), int(hour), int(minute), int(second), 0, 0, 0)
+        time_sec = time.mktime(tup)
+        time_sec += 15 * 60 * 60 # 2 weeks
 
+        return time.strftime("%Y%m%d", time.gmtime(time_sec))
+
+    def Out(self):
+        pauta = Pautas()
+        data = pauta.GetContentFromRaw(self.webURL)
+        output = ""
+        for section in data:
+            secondLine = "=" * len(section)
+            output += section + "\n"
+            output += secondLine + "\n"
+            if re.search("Assuntos", section):
+                output += "\n"
+                continue
+            output += "\n".join(data[section])
+            output += "\n\n"
+        print(output)
+        timestamp = self.CalculateNextDay()
+        print("Next episode:", timestamp)
+        with open(f"{timestamp}.md", "w") as dest:
+            dest.write(output)
+        print(f"Saved into: {timestamp}.md")
 
 if __name__ == '__main__':
     filter = NewFilter()
